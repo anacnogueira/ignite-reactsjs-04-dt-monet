b463f67ce6685375d79ec88dcb7bd97568599c2b
Ana Claudia <anacnogueira@gmail.com>
2024-02-18 12:03:15 -0300

    Componente SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import {
   PriceHighlight,
   TransactionContainer,
@@ -12,6 +13,7 @@ export function Transactions() {
       <Header></Header>
       <Summary />
       <TransactionContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
